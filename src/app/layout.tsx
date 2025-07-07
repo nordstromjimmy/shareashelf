164101e44f6d5c83ec5192868b02389ce73f1f20
Jimmy <nordstromjimmy@gmail.com>
2025-07-07 18:40:41 +0200

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>TopShelfy</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <script
+          defer
+          data-domain="topshelfy.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      </head>
       <body>
         {children}
         <Toaster position="bottom-center" />
