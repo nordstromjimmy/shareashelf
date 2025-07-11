6add4ce2082e598e0f528623c8f2ad57021411cc
Jimmy <nordstromjimmy@gmail.com>
2025-07-11 21:09:44 +0200

    update

--- src/app/shelf/[shelfId]/bottle/[bottleId]/page.tsx
+++ src/app/shelf/[shelfId]/bottle/[bottleId]/page.tsx
@@ -23,21 +23,6 @@ export default async function BottleDetailsPage({
   return (
     <main className="min-h-screen bg-zinc-900 text-white px-6 py-12 flex flex-col items-center">
       <div className="max-w-2xl w-full">
-        <div className="flex flex-col sm:flex-row justify-between items-center mb-6 gap-4">
-          <Link
-            href={`/shelf/${shelfId}`}
-            className="inline-block bg-orange-600 hover:bg-orange-700 py-2 px-6 rounded-xl text-lg font-semibold transition shadow hover:shadow-orange-600/40"
-          >
-            Back
-          </Link>
-          <Link
-            href={`/edit/${bottle.id}`}
-            className="inline-block bg-orange-600 hover:bg-orange-700 py-2 px-6 rounded-xl text-lg font-semibold transition shadow hover:shadow-orange-600/40"
-          >
-            Edit bottle
-          </Link>
-        </div>
-
         <div className="bg-zinc-800 p-6 rounded-xl shadow mb-6 relative">
           <div className="relative mb-6">
             {bottle.top_shelf && (
@@ -97,6 +82,20 @@ export default async function BottleDetailsPage({
               </p>
             </div>
           )}
+        </div>
+        <div className="flex flex-row justify-between items-center mb-6 gap-4">
+          <Link
+            href={`/shelf/${shelfId}`}
+            className="inline-block bg-orange-600 hover:bg-orange-700 py-2 px-6 rounded-xl text-lg font-semibold transition shadow hover:shadow-orange-600/40"
+          >
+            Back
+          </Link>
+          <Link
+            href={`/edit/${bottle.id}`}
+            className="inline-block bg-orange-600 hover:bg-orange-700 py-2 px-6 rounded-xl text-lg font-semibold transition shadow hover:shadow-orange-600/40"
+          >
+            Edit bottle
+          </Link>
         </div>
       </div>
     </main>
